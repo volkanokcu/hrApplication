8e86a020437aa1c2bf4c021f2f34c4d25a55082d
volkanokcu <archervolcano@gmail.com>
2018-02-18 01:04:14 +0300

    Last Update Meybe?

--- hrApplication/src/main/java/com/hr/rest/JobAdverismentREST.java
+++ hrApplication/src/main/java/com/hr/rest/JobAdverismentREST.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hr.entity.impl.JobAdvertisement;
@@ -44,10 +43,27 @@ public class JobAdverismentREST {
 		return new ResponseEntity<>(new CustomResponse(HttpStatus.CREATED.toString()), HttpStatus.CREATED);
 	}
 	
-	@PutMapping("auth/job/{id}")
-	public ResponseEntity<HttpStatus> put(@RequestBody JobAdvertisement entity) {
-		
-		return new ResponseEntity<>(HttpStatus.CREATED);
+	@PutMapping("auth/job")
+	public ResponseEntity<CustomResponse> put(@RequestBody JobAdvertisement entity) {
+		JobAdvertisement advertisement = service.findById(entity.getId());
+		if (advertisement == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else { 
+			service.save(entity);
+			return new ResponseEntity<>(new CustomResponse(HttpStatus.OK.toString()), HttpStatus.OK);
+		}
+	}
+	
+	@DeleteMapping("auth/job/{id}") 
+	public ResponseEntity<CustomResponse> delete(@PathVariable Integer id) {
+		JobAdvertisement advertisement = service.findById(id);
+		if (null == advertisement) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			service.delete(advertisement);
+			return new ResponseEntity<>(new CustomResponse(HttpStatus.OK.toString()), HttpStatus.OK);
+		}
 	}
 	
 
