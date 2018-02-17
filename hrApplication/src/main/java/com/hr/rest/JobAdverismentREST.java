1bccbeaac51a09c9b72abaeb69d4923dcf10a466
volkanokcu <archervolcano@gmail.com>
2018-02-17 23:31:14 +0300

    Updates

--- hrApplication/src/main/java/com/hr/rest/JobAdverismentREST.java
+++ hrApplication/src/main/java/com/hr/rest/JobAdverismentREST.java
@@ -1,9 +1,12 @@
 package com.hr.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hr.entity.impl.JobAdvertisement;
+import com.hr.model.CustomResponse;
 import com.hr.service.JobAdvertisementService;
 
 @RestController
@@ -25,21 +29,19 @@ public class JobAdverismentREST {
 	}
 	
 	@GetMapping("job/details/{id}")
-	public ResponseEntity<JobAdvertisement> get(@RequestParam Integer id) {
-		
-		return new ResponseEntity<>(null, HttpStatus.OK);
+	public ResponseEntity<JobAdvertisement> get(@PathVariable Integer id) {
+		return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
 	}
 	
 	@GetMapping("job/list")
-	public ResponseEntity<JobAdvertisement> getList() {
-		
-		return new ResponseEntity<>(null, HttpStatus.OK);
+	public ResponseEntity<List<JobAdvertisement>> getList() {
+		return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
 	}
 	
 	@PostMapping("auth/job")
-	public ResponseEntity<HttpStatus> post(@RequestBody JobAdvertisement entity) {
-		
-		return new ResponseEntity<>(HttpStatus.CREATED);
+	public ResponseEntity<CustomResponse> post(@RequestBody JobAdvertisement entity) {
+		service.save(entity);
+		return new ResponseEntity<>(new CustomResponse(HttpStatus.CREATED.toString()), HttpStatus.CREATED);
 	}
 	
 	@PutMapping("auth/job/{id}")
